e000e42e697d67977e314f109f2dd88dd1dbb9a6
Leonardo-12953 <leosantos12953@gmail.com>
2026-09-09 15:57:11 -0300

    Aula de listas

---
+++ aula08/src/Kunai.java
@@ -0,0 +1,16 @@
+public class Kunai {
+    private String nome;
+
+    public Kunai(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    @Override
+    public String toString () {
+        return "Kunai: "+ nome;
+    }
+}
